f99e236bb2d42affdd10f8501ac52fe5a740cf04
itheima <1305975814@qq.com>
2022-05-18 23:30:10 +0800

    第二次

---
+++ heima-leadnews-admin/src/main/java/com/heima/admin/service/impl/AdChannelServiceImpl.java
@@ -0,0 +1,44 @@
+package com.heima.admin.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.heima.admin.mapper.AdChannelMapper;
+import com.heima.admin.service.AdChannelService;
+import com.heima.model.admin.dtos.ChannelDto;
+import com.heima.model.admin.pojos.AdChannel;
+import com.heima.model.common.dtos.PageResponseResult;
+import com.heima.model.common.dtos.ResponseResult;
+import com.heima.model.common.enums.AppHttpCodeEnum;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AdChannelServiceImpl extends ServiceImpl<AdChannelMapper, AdChannel> implements AdChannelService {
+
+
+    @Override
+    public ResponseResult findByNameAndPage(ChannelDto dto) {
+
+        //1.参数检测
+        if (dto == null) {
+            return ResponseResult.errorResult(AppHttpCodeEnum.PARAM_INVALID);
+        }
+        //分页参数检查
+        dto.checkParam();
+
+        //2.按照名称模糊分页查询
+        Page page = new Page(dto.getPage(), dto.getSize());
+        LambdaQueryWrapper<AdChannel> lambdaQueryWrapper = new LambdaQueryWrapper();
+        if (StringUtils.isNotBlank(dto.getName())) {
+            lambdaQueryWrapper.like(AdChannel::getName, dto.getName());
+        }
+        IPage result = page(page, lambdaQueryWrapper);
+
+        //3.结果封装
+        ResponseResult responseResult = new PageResponseResult(dto.getPage(), dto.getSize(), (int) result.getTotal());
+        responseResult.setData(result.getRecords());
+        return responseResult;
+    }
+}
